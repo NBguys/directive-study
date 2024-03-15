996a9a7b6f2821fedfd069b43a3038b16b0d92b5
NBguys <gudwns520@gamil.com>
2024-03-15 15:31:55 +0900

    속성 바인딩 활용

--- src/app/better-highlight/better-highlight.directive.ts
+++ src/app/better-highlight/better-highlight.directive.ts
@@ -1,22 +1,28 @@
-import { Directive, OnInit, Renderer2, ElementRef, HostListener } from '@angular/core';
+import { Directive, OnInit, Renderer2, ElementRef, HostListener, HostBinding, Input } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
 })
 export class BetterHighlightDirective implements OnInit {
+  @Input() defalutColor: string = "transparent";
+  @Input() highlightColor: string = "blue";
+  @HostBinding('style.backgroundColor') backgroundColor: string = "transparent";
 
   constructor(private elRef: ElementRef, private renderer: Renderer2) { }
 
   ngOnInit() {
+    this.backgroundColor = this.defalutColor;
     // this.renderer.setStyle(this.elRef.nativeElement , 'background-color', 'blue');
   }
 
   @HostListener('mouseenter') mouseover(eventData: Event) {
-    this.renderer.setStyle(this.elRef.nativeElement , 'background-color', 'blue');
+    // this.renderer.setStyle(this.elRef.nativeElement , 'background-color', 'blue');
+    this.backgroundColor = this.highlightColor;
   }
 
   @HostListener('mouseleave') mouseleave(eventData: Event) {
-    this.renderer.setStyle(this.elRef.nativeElement , 'background-color', 'transparent');
+    // this.renderer.setStyle(this.elRef.nativeElement , 'background-color', 'transparent');
+    this.backgroundColor = 'transparent';
   }
 
 }
